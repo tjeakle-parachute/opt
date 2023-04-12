c3a76eb5ab2d9264e7f55656a190e075143651c1
tjeakle-parachute <109230738+tjeakle-parachute@users.noreply.github.com>
2023-04-12 10:51:13 -0500

    commit before removing a bunch of code.

--- script/adhoc/change_plan_plan_family_matches.rb
+++ script/adhoc/change_plan_plan_family_matches.rb
@@ -20,34 +20,20 @@ CARRIER_WORDS = %w[blue bluecross blueshield bcbs cross shield unitedhealthcare 
 conn = PG.connect(dbname: 'change_payers', user: 'postgres', password: 'postgres')
 
 # plan_families = conn.exec("SELECT * FROM plan_families WHERE id = '28'")
-plan_families = conn.exec("SELECT * FROM plan_families WHERE id = '839' ORDER BY TO_NUMBER(COALESCE(last_column, '0') , '99999999999') desc limit 10")
+
+plan_family_sql = "SELECT *
+                    FROM plan_families
+                    WHERE last_column IS NOT NULL
+                    AND plan_type = 'commercial'
+                    ORDER BY TO_NUMBER(last_column, '99999999999') desc
+                    LIMIT 5"
+
+plan_families = conn.exec(plan_family_sql)
 change_plans = conn.exec('SELECT * FROM change_plans')
 
-# plan_families.each do |pf|
-#   pf['matches'] = []
-#   pf['total_words'] = 0
-#   plan_family_words = conn.exec("SELECT * FROM plan_family_words WHERE plan_family_id = '#{pf['id']}'")
-#   plan_family_words.each do |pfw|
-#     puts "for word #{pfw.word}"
-#     change_plans.each do |cp|
-#       words_matched = 0
-#       change_plan_words = conn.exec("SELECT DISTINCT word, payer_name FROM change_plan_words WHERE payer_name = '#{conn.escape_string(cp['payer_name'])}'")
-#       change_plan_words.each do |cpw|
-#         words_matched += 1 if pfw['word'] == cpw['word']
-#       end
-#       next unless words_matched.positive?
-
-#       h = {}
-#       h['matched_changed_plan'] = cp
-#       h['matched_words'] = words_matched
-#       pf['matches'].push h
-#     end
-#     puts 'searched '
-#     pf['total_words'] += 1
-#   end
-# end
 CSV.open('change_plan_plan_family_matches.csv', 'w') do |csv|
-  csv << %w[id total_orders name match_score change_plan_payer_name change_payer_id]
+  csv << %w[id total_orders name type match_score change_plan_payer_name change_payer_id change_insurance_type
+            payer_id_word_score change_plan_word_score]
   plan_families.each do |pf|
     pf['matches'] = []
     pf['payer_ids'] = []
@@ -83,30 +69,38 @@ CSV.open('change_plan_plan_family_matches.csv', 'w') do |csv|
                                  1
                                end
           # INSURANCE_TYPES = %w[MEDICARE MEDICAID HMO BLUE_CROSS TRICARE WORKERS_COMPENSATION].freeze
-
-          if %w[HMO BLUE_CROSS WORKERS_COMPENSATION
-                TRICARE].include? cp['insurance_type'] && pf['plan_type'] == 'commercial'
-            cp['match_score'] += 1
-          elsif cp['insurance_type'] == 'MEDICARE' && pf['plan_type'] == 'medicare'
-            cp['match_score'] += 1
-          elsif cp['insurance_type'] == 'MEDICAID' && pf['plan_type'] == 'medicaid'
-            cp['match_score'] += 1
-          end
         end
       end
       iteration_counter += 1
+
       next unless (cp['words_matched']).positive?
 
-      if pf['words'] == cp['words_matched'] && pf['words'] > 1
-        cp['match_score'] += 2
-      elsif cp['words_matched'] > 1
+      if %w[HMO BLUE_CROSS WORKERS_COMPENSATION
+            TRICARE].include?(cp['insurance_type']) && pf['plan_type'] == 'commercial'
+        cp['match_score'] += 1
+      elsif cp['insurance_type'] == 'MEDICARE' && pf['plan_type'] == 'medicare'
+        cp['match_score'] += 1
+      elsif cp['insurance_type'] == 'MEDICAID' && pf['plan_type'] == 'medicaid'
         cp['match_score'] += 1
       end
+
+      if pf['words'] == cp['words_matched'] && pf['words'] > 1
+        cp['match_score'] += 3
+      elsif cp['words_matched'] > 1
+        cp['match_score'] += 2
+      end
+
+      unless cp['payer_id'].nil?
+        cp['payer_id_match_score'] =
+          cp['payer_id'].downcase.pair_distance_similar(pf['name'].downcase)
+      end
+      cp['name_change_plan_match_score'] = pf['name'].downcase.pair_distance_similar(cp['payer_name'].downcase)
 
       pf['payer_ids'].push cp['payer_id'] unless cp['payer_id'].nil?
       pf['plan_names'].push cp['payer_name']
       pf['matches'].push cp
-      csv << [pf['id'], pf['last_column'], pf['name'], cp['match_score'], cp['payer_name'], cp['payer_id']]
+      csv << [pf['id'], pf['last_column'], pf['name'], pf['plan_type'], cp['match_score'], cp['payer_name'],
+              cp['payer_id'], cp['insurance_type'], cp['payer_id_match_score'], cp['name_change_plan_match_score']]
 
       # puts "Match found for plan_family #{pf['name']} and change payer #{cp['payer_name']} with #{cp['words_matched']} matched word."
     end
